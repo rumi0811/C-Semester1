54b205594ee80d0bc78809e45ff59d7992f37d8e
Sekar Puspita Arum <94540556+rumi0811@users.noreply.github.com>
2021-12-30 20:44:11 +0700

    Create tugas-pertemuan5-3.cpp

---
+++ tugas-pertemuan5-3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int a, i, j, jum;
+  
+      for (i=1;i<=4; i++)
+      {
+        jum=0;
+        for (j=1;j<=i;j++)
+        {
+            if (j!=1)
+            cout << "+";
+            cout << j;
+            jum+=j;
+        }
+      cout << "\t\t= "<<jum<<endl;
+      }
+  
+      for (i=5;i==5;i++)
+      {
+        jum=0;
+        for (j=1;j<=i;j++)
+        {
+          if(j!=1)
+          cout<<"+";
+          cout<<j;
+          jum+=j;
+        }
+      }
+ cout<<"\t= "<<jum<<endl;
+}
