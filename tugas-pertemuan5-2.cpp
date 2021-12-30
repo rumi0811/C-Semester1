a32967cf376da796905b0ad35dfa44894d851865
Sekar Puspita Arum <94540556+rumi0811@users.noreply.github.com>
2021-12-30 20:40:59 +0700

    Create tugas-pertemuan5-2.cpp

---
+++ tugas-pertemuan5-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+  int a=1, b=19, n;
+  for(a=a;a<=b;a+=2)
+  {
+      cout << a;
+      if(a<b)
+  {
+cout<<"+";
+}
+}
+  cout<<"=";
+  n=(b+1)/2;
+  n=n*n;
+  cout<<n;
+  
+getch();
+return 0;
+}
