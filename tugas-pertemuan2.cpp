a7632922ac379aedd5cf6a0720ff4ca21b7584a6
Sekar Puspita Arum <94540556+rumi0811@users.noreply.github.com>
2021-12-30 20:09:09 +0700

    membuat tugas pertemuan 2 file c++.cpp

---
+++ tugas-pertemuan2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>>
+using namespace std;
+int main()
+{
+ string nama [40];
+ int npm;
+ printf("Nama \t:");scanf("%s",&nama);
+ //printf ("Nama \t:");gets(nama);
+ printf("NPM \t:");scanf("%i",&npm);
+ //printf ("NPM \t:");gets(npm);
+ printf("\n");
+ printf("Nama saya adalah %s",nama);
+ printf("\n");
+ printf("NPM saya adalah %i",npm);
+ return 0;
+}
